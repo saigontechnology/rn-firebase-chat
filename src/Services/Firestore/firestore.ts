afc29f30131f854c179f00064a5bb6f7367c55b6
ZanBin <150883191+ZanBin@users.noreply.github.com>
2023-12-28 18:47:01 +0700

    Revert "Update firestore.ts"
    This reverts commit ffd9a2b35ad27e784858a59692c4e9ff18768ef7.

--- src/Services/Firestore/firestore.ts
+++ src/Services/Firestore/firestore.ts
@@ -373,6 +373,7 @@ export class FirestoreServices {
       updated: new Date().valueOf(),
       unRead: {
         [userId]: 0,
+        // [memberId]: 0,
       },
       members: conversationData.members,
     };
