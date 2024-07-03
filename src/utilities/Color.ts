80f8da3639bb5208c81414903f28d2e6f55eff05
loc-nguyent-sts <114563576+loc-nguyenthien@users.noreply.github.com>
2024-07-03 20:53:46 +0700

    chore: fix lint and update Podfile.lock (#41)

--- src/utilities/Color.ts
+++ src/utilities/Color.ts
@@ -1,12 +1,14 @@
 function hashCode(str: string) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
+    // eslint-disable-next-line no-bitwise
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
   return hash;
 }
 
 function intToRGB(i: number) {
+  // eslint-disable-next-line no-bitwise
   const c = (i & 0x00ffffff).toString(16).toUpperCase().padStart(6, '0');
   return `#${c}`;
 }
