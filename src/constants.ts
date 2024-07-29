7f3ea9b2a2addd4cd40fe80df299420213faff9c
huydosgtech <105196859+huydosgtech@users.noreply.github.com>
2024-07-29 15:29:32 +0700

    feat: [FC-8] config web hook push notification (#29)
    * feat: [FC-8] config web hook push notification
    
    * fix: rename default notification
    
    ---------
    
    Co-authored-by: HuyDo <huy.do@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@
 export const DEFAULT_SALT: string = 'salt';
 export const DEFAULT_ITERATIONS: number = 5000;
 export const DEFAULT_KEY_LENGTH: number = 256;
+export const DEFAULT_CLEAR_SEND_NOTIFICATION: number = 3000;
