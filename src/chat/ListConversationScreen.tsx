96305df4064369b95840c71a2b823e7b2cdb07b5
danhmaisgt <danh.mai@saigontechnology.com>
2024-07-31 17:01:55 +0700

    fix: format conversation latest message text

--- src/chat/ListConversationScreen.tsx
+++ src/chat/ListConversationScreen.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useMemo } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import {
   ConversationItem,
   IConversationItemProps,
 } from './components/ConversationItem';
-import type { ConversationProps } from '../interfaces';
+import { MessageTypes, type ConversationProps } from '../interfaces';
 import { useChatContext, useChatSelector } from '../hooks';
 import { setConversation } from '../reducer';
 import { getListConversation } from '../reducer/selectors';
@@ -43,11 +44,44 @@ export const ListConversationScreen: React.FC<IListConversationProps> = ({
     [chatDispatch, onPress]
   );
 
+  const getInitialsChat = (item: ConversationProps): string => {
+    if (!item?.latestMessage || !item?.latestMessage?.type) return '';
+
+    const { latestMessage } = item;
+    const { senderId, text } = latestMessage;
+    const isCurrentUser = senderId === userInfo?.id;
+
+    const getMessageText = (messageType: string): string => {
+      const senderInfo = `${
+        isCurrentUser ? 'You' : item.latestMessage?.name
+      } sent a`;
+
+      switch (messageType) {
+        case MessageTypes.text:
+          return text;
+        case MessageTypes.image:
+          return `${senderInfo} photo`;
+        case MessageTypes.video:
+          return `${senderInfo} video`;
+        case MessageTypes.voice:
+          return `${senderInfo} voice message`;
+        default:
+          return '';
+      }
+    };
+
+    return getMessageText(item?.latestMessage?.type);
+  };
+
   const renderItem = useCallback(
     ({ item, index }: ListItem) => {
+      const formattedItem = {
+        ...item,
+        latestMessage: { ...item.latestMessage, text: getInitialsChat(item) },
+      } as ConversationProps;
       if (renderCustomItem)
         return renderCustomItem({
-          item,
+          item: formattedItem,
           index,
         });
       return (
@@ -61,6 +95,7 @@ export const ListConversationScreen: React.FC<IListConversationProps> = ({
     },
     [
       conversationItemProps,
+      getInitialsChat,
       handleConversationPressed,
       renderCustomItem,
       userInfo,
