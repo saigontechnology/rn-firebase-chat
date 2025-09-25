ac6f51346911c7b958e2a1e9f13c78fbad9a2402
Loc NguyenThien <loc.nguyent@saigontechnology.com>
2025-09-25 13:18:25 +0700

    fix: random value error and update README

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,17 @@
 // constants.ts
-import 'react-native-get-random-values';
-import { v4 as uuidv4 } from 'uuid';
 
-export const DEFAULT_SALT: string = uuidv4(); // Generate a unique salt
+const cryptoRandomUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+    /[xy]/g,
+    (c: string) => {
+      var r = (Math.random() * 16) | 0;
+      var v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    }
+  );
+};
+
+export const DEFAULT_SALT: string = cryptoRandomUUID(); // Generate a unique salt
 export const DEFAULT_ITERATIONS: number = 10000; // Increased for better security
 export const DEFAULT_KEY_LENGTH: number = 256;
 export const DEFAULT_CLEAR_SEND_NOTIFICATION: number = 3000;
