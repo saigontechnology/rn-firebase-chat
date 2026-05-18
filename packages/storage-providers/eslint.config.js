866d70cbe0524fd91e0c0109b74ef2348118d10d
loc-nguyent-sts <loc.nguyent@saigontechnology.com>
2026-05-18 13:37:11 +0700

    chore: fix typescript config for eslint

--- packages/storage-providers/eslint.config.js
+++ packages/storage-providers/eslint.config.js
@@ -15,6 +15,7 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
